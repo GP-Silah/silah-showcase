d4f181e8fe1350ca30b21e7631771da9a6ed182a
Codyn001 <Codyn001.dev@proton.me>
2026-01-02 13:34:45 +0300

    all components are done الحمد لله

--- src/utils/mock-api/categoryApi.js
+++ src/utils/mock-api/categoryApi.js
@@ -1,9 +1,16 @@
 import { API_BASE_URL } from '@/config/api';
 
-export function getCategories({ main = false, lang = 'en' }) {
+export function getCategories({
+  main = false,
+  lang = 'en',
+  sub = false,
+  type = 'null',
+}) {
   if (main) {
     return `${API_BASE_URL}/categories/main.${lang}.json`;
   }
-
+  if (sub) {
+    return `${API_BASE_URL}/categories/sub-${type}.${lang}.json`;
+  }
   return `${API_BASE_URL}/categories/all.${lang}.json`;
 }
