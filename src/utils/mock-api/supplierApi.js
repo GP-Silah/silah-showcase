86fabf131a6167775f7bd8a062fb379f74d64acf
Codyn001 <Codyn001.dev@proton.me>
2026-01-07 12:33:19 +0300

    started working on supplier pages

--- src/utils/mock-api/supplierApi.js
+++ src/utils/mock-api/supplierApi.js
@@ -3,3 +3,19 @@ import { API_BASE_URL } from '@/config/api';
 export function getFavoriteCategories() {
   return `${API_BASE_URL}/suppliers/favorite-categories.json`;
 }
+
+export function getSupplier() {
+  return `${API_BASE_URL}/suppliers/me.json`;
+}
+
+export function getPendingOrdersCount() {
+  return `${API_BASE_URL}/supplieres/pending-orders-count.json`;
+}
+
+export function getStockLevels() {
+  return `${API_BASE_URL}/suppliers/stock-levels.json`;
+}
+
+export function getPlan() {
+  return `${API_BASE_URL}/suppliers/plan.json`;
+}
